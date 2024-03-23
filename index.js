a8735f62d2eea4faa641d0892ad813435c7b44c5
SyedEbad902 <ebad3088@gmail.com>
2024-03-24 00:21:14 +0500

    Number guessing game using typescript

---
+++ index.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+let randomNum = Math.floor(Math.random() * 10);
+console.log(randomNum);
+let userAnswer = await inquirer.prompt([
+    { message: "Guess  any  number", name: "userValue", type: "number" }
+]);
+if (userAnswer.userValue === randomNum) {
+    console.log("Congratulation! you won the game");
+}
+else {
+    console.log("You guess the wrong number");
+}
